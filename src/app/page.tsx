d92dc49459a8d6d32b6ae1de01ee1d7e33e0d397
Andressa <andressavianab@gmail.com>
2023-05-18 11:34:23 -0300

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50">
       <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h2>Testindo new remote repo</h2>
     </div>
   )
 }
